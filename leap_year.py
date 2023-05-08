334ada54ca05a0821c1c71517254771c609b4a5e
Hilya Ntinda <113698301+Hilya02@users.noreply.github.com>
2023-05-08 09:23:39 +0200

    leap_year.py

---
+++ leap_year.py
@@ -0,0 +1,6 @@
+# Write function `is_leap_year` that takes a year as a parameter
+#   -->**RETURNS**<-- True if the year is a leap year, False if not.
+# The logic-chain is somewhat similar to the Sherlock problem.
+
+def is_leap_year(year):
+    return ''
